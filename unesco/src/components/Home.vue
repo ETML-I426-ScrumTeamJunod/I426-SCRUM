01d57364447149b8587396a7118f45efb47f8cb5
Aaron Vichery <pa84igb@eduvaud.ch>
2026-02-06 15:41:37 +0100

    feat(frontend-site):Début du frontend du site

---
+++ unesco/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <img src="../assets/unesco.png" />
+    <ul>
+      <li>
+        <button>Accueil</button>
+        <button>Accueil</button>
+        <button>Accueil</button>
+        <button>Accueil</button>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped></style>
